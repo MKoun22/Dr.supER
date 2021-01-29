acb7c4a5b56c05834cee562e296435b016767d51
MKoun22 <mkoun22@gmail.com>
2021-01-29 14:26:00 +0200

    Main Menu Scene

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+{
+    
+    public Transform dist;
+    private float distance;
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, dist.position);
+        Debug.Log(distance);
+        if (distance < 1.5f)
+        {
+            LoadA("Scene2");
+        }
+    }
+    public void LoadA(string scene_name)
+    {
+        Debug.Log("sceneName to load:" + scene_name);
+        SceneManager.LoadScene(scene_name);
+    }
+
+    /*public float Example()
+    {
+        if(theDest)
+        {
+            float dist = Vector3.Distance(theDest.position, transform.position);
+            print("Distance to other:" + dist);
+            return dist;
+        }
+
+
+
+    }*/
+    //public static float Distance(Vector3 a, Vector3 b)
+   // {
+        //if (distance < 0.7f)
+        //{
+
+       // }
+  //  }
+}
---
+++ Assets/ChangeScene2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene2 : MonoBehaviour
+{
+    public Transform dist;
+    private float distance;
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, dist.position);
+        Debug.Log(distance);
+        if (distance < 1.5f)
+        {
+            LoadA("Scene1");
+        }
+    }
+    public void LoadA(string scene_name)
+    {
+        Debug.Log("sceneName to load:" + scene_name);
+        SceneManager.LoadScene(scene_name);
+    }
+}
+    /*    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        //Input.GetMouseButtonDown(0)
+        {
+            //Debug.Log("LoadSceneA");
+            LoadA("Scene1");
+        }
+    }
+    public void LoadA(string scene_name)
+    {
+        Debug.Log("sceneName to load:" + scene_name);
+        SceneManager.LoadScene(scene_name);
+    }
+}*/
